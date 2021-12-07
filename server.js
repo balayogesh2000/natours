8cd2b58064f196e21dd706db69fa62c9f7e70429
balayogesh <balayogesh2000@gmail.com>
2021-12-07 15:26:03 +0530

    port changed

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
 
 const app = require("./app");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () =>
   console.log(`listening on the port ${port}`)
 );
